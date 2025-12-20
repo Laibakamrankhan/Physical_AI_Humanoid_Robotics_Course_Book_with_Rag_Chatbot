548107b5eef31a7169baefa9cc2a8dd986f18488
Laiba Kamran Khan <160596709+Laibakamrankhan@users.noreply.github.com>
2025-12-20 14:58:48 +0500

    Update api.js

--- src/components/ChatWidget/api.js
+++ src/components/ChatWidget/api.js
@@ -15,6 +15,7 @@ class ChatAPI {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          "Authorization": `Bearer ${process.env.REACT_APP_API_KEY}`,
         },
         body: JSON.stringify({
           message,
